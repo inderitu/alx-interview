4feb63433703afa984003f4af83b1148f4fa7438
Innocent-Bern <innocentbern22@gmail.com>
2024-07-11 21:59:59 +0300

    utf8 validation

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+utf8 validation
+"""
+
+
+def validUTF8(data):
+    """
+    returns true if data is uft8 valid
+    """
+
+    byte_count = 0
+
+    for i in data:
+        if byte_count == 0:
+            if i >> 5 == 0b110 or i >> 5 == 0b1110:
+                byte_count = 1
+            elif i >> 4 == 0b1110:
+                byte_count = 2
+            elif i >> 3 == 0b11110:
+                byte_count = 3
+            elif i >> 7 == 0b1:
+                return False
+        else:
+            if i >> 6 != 0b10:
+                return False
+            byte_count -= 1
+    return byte_count == 0
